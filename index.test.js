b47a6947ca7f6cfd783961a4960e28ff1125a352
Matteo Sette <msette8@gmail.com>
2019-11-14 22:47:48 +0100

    test redirects

--- index.test.js
+++ index.test.js
@@ -2,60 +2,76 @@ const supertest = require("supertest");
 const app = require("./index");
 const cookieSession = require("cookie-session");
 
-//Mochhere the user cookieSession
-cookieSession.mockSession({
-    userId: 10
-});
-
-test("Get /welcome sends html", () => {
-    return supertest(app)
-        .get("/welcome")
-        .then(res => {
-            console.log(res.statusCode, res.text);
-        });
-});
-
-test("GET /welcome sends html", () => {
-    return supertest(app)
-        .get("/welcome")
-        .then(res => {
-            expect(res.statusCode).toBe(200);
-            expect(res.text).toContain("<h1>Welcome!</h1>");
-        });
-});
-
-test("GET /home redirects if session is not right", () => {
-    return supertest(app)
-        .get("/home")
-        .then(res => {
-            expect(res.statusCode).toBe(302);
-            expect(res.headers.location).toBe("/welcome");
-        });
-});
-
-test("GET /home sends html if session is right", () => {
-    cookieSession.mockSessionOnce({
-        submitted: 1
+test("Get /petition with no auth gets redirects to Registration", () => {
+    cookieSession.mockSession({
+        userId: null
     });
     return supertest(app)
-        .get("/home")
+        .get("/petition")
         .then(res => {
-            expect(res.statusCode).toBe(200);
-            expect(res.text).toContain("<title>Home</title>");
+            console.log(res.statusCode);
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe("/register");
         });
 });
 
-test("POST /welcome sets req.session.submitted and redirects to /home", () => {
-    const mySession = {};
-    cookieSession.mockSessionOnce(mySession);
+test("Get /register with auth", () => {
+    cookieSession.mockSession({
+        user: {
+            userId: 10
+        }
+    });
     return supertest(app)
-        .post("/welcome")
-        .send("funky=chicken")
+        .get("/register")
         .then(res => {
+            console.log(res.statusCode);
             expect(res.statusCode).toBe(302);
-            expect(res.headers.location).toBe("/home");
-            expect(mySession).toEqual({
-                submitted: true
-            });
+            expect(res.headers.location).toBe("/petition");
+        });
+});
+
+test("Get /petition with auth plus signature", () => {
+    cookieSession.mockSession({
+        user: {
+            userId: 10,
+            signatureId: 12
+        }
+    });
+    return supertest(app)
+        .get("/petition")
+        .then(res => {
+            console.log(res.statusCode);
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe("/petition/signed");
+        });
+});
+
+test("Get /petition/signed with auth, but no signature", () => {
+    cookieSession.mockSession({
+        user: {
+            userId: 10
+        }
+    });
+    return supertest(app)
+        .get("/petition/signed")
+        .then(res => {
+            console.log(res.statusCode);
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe("/petition");
+        });
+});
+
+test("Get /petition/signers with auth, but no signature", () => {
+    cookieSession.mockSession({
+        user: {
+            userId: 10
+        }
+    });
+    return supertest(app)
+        .get("/petition/signers")
+        .then(res => {
+            console.log(res.statusCode);
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe("/petition");
         });
 });
