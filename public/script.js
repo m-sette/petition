cb5e432ffbceb652ea952559fd2f40639feab3f3
Matteo Sette <msette8@gmail.com>
2019-11-16 14:49:52 +0100

    canvas plus style

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 (function() {
     let canvas = $("#canvas-body");
-    let x, y;
-    let drawing = false;
     let c = canvas[0].getContext("2d");
     const rect = canvas[0].getBoundingClientRect();
     const sign = $("#sig");
     const submit = $("#submit");
     let form = $(".registration");
 
-    submit.on("click", e => {
-        console.log(e);
+    let x = 0;
+    let y = 0;
+    let drawing = false;
 
+    submit.on("click", e => {
         if (!form[0][0].value) {
             $("#error").css("visibility", "visible");
             e.preventDefault();
@@ -18,9 +18,9 @@
     });
 
     canvas.on("mousedown", e => {
-        drawing = true;
         x = e.clientX - rect.left;
         y = e.clientY - rect.top;
+        drawing = true;
     });
 
     canvas.on("mousemove", e => {
@@ -31,14 +31,21 @@
         }
     });
 
-    canvas.on("mouseup", () => {
-        drawing = false;
+    canvas.on("mouseup", e => {
+        if (drawing === true) {
+            draw(c, x, y, e.clientX - rect.left, e.clientY - rect.top);
+            x = 0;
+            y = 0;
+            drawing = false;
+        }
         const dataURL = canvas[0].toDataURL();
         sign.val(dataURL);
     });
 
     function draw(c, x1, y1, x2, y2) {
         c.beginPath();
+        c.strokeStyle = "blue";
+        c.lineWidth = 2;
         c.moveTo(x1, y1);
         c.lineTo(x2, y2);
         c.stroke();
